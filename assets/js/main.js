2658006bc8122b41962c3d82e2b41b25ebcf60a1
Ramraghul <raghulraghul111@gmail.com>
2024-01-04 12:27:16 +0530

    Email Template Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,20 +4,20 @@ const showMenu = (toggleId, navId) => {
       const toggle = document.getElementById(toggleId),
             nav = document.getElementById(navId)
 
-      if(toggle && nav){
-            toggle.addEventListener('click', () =>{
+      if (toggle && nav) {
+            toggle.addEventListener('click', () => {
                   nav.classList.toggle('show')
             });
       }
 }
 
-showMenu('nav_toggle','nav_menu')
+showMenu('nav_toggle', 'nav_menu')
 
 // ACTIVE & REMOVE ACTIVE
 const navLink = document.querySelectorAll('.nav_link')
 navLink.forEach(n => n.classList.remove('active'))
 
-function linkAction(){
+function linkAction() {
       navLink.forEach(n => n.classList.remove('active'))
       this.classList.add('active')
 
@@ -25,4 +25,19 @@ function linkAction(){
       navMenu.classList.remove('show')
 }
 
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+// EMAIL CONTENT OPTION
+
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+      const subjectInputValue = document.getElementById('subjectInput').value;
+      const nameInputValue = document.getElementsByName('Name')[0].value;
+      const messageInputValue = document.getElementsByName('Message')[0].value;
+
+      const bodyContent = `Hi Raghul,\n\nI am ${nameInputValue}. I hope this email finds you.\n\n${messageInputValue}`;
+
+      const mailtoLink = `mailto:raghulraghul111@gmail.com?subject=${encodeURIComponent('Reg: ' + subjectInputValue)}&body=${encodeURIComponent(bodyContent)}`;
+
+      window.location.href = mailtoLink;
+      event.preventDefault(); // Prevents the default form submission
+});
